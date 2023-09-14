bc2921f82dfe7700387fbec0b62039142a2b700a
Rickson <ricksonrocha0@gmail.com>
2023-09-14 17:58:17 -0300

    feat: troca da api para homologação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  BASE_URL: "http://localhost:8080/api/",
-  // BASE_URL: "http://localhost:3000/",
+  // BASE_URL: "http://localhost:8080/api/",
+  BASE_URL: "http://localhost:3000/",
 };
