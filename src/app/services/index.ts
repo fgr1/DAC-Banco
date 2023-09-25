6565080441be7e63f8b4640831da9b1904123a79
Rickson Rocha <71469864+RicksonRocha@users.noreply.github.com>
2023-09-25 19:46:41 -0300

    Revert "Front do CRUD de gerentes adicionado"

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,4 +2,3 @@ export { AuthService } from "./auth.service";
 export { BankAccountService } from "./bank-account.service";
 export { CepService } from "./cep.service";
 export { ClientService } from "./client.service";
-export { AdministratorService } from "./administrator.service";
